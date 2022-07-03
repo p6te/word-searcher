b99658c776af4a86e92d2ca3d7967b33af4a1de5
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-07-03 11:39:46 +0200

    add searcher bottom-vertical.

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -88,6 +88,49 @@ class WordSearch {
     } else return;
   }
 
+  searcherBottomTop(word) {
+    //trun the array for 90 deg
+    let verticalArray = [];
+    let addRow = "";
+    this.letterGrid[0].length;
+
+    for (let i = 0; i < this.letterGrid[0].length; i++) {
+      this.letterGrid.forEach((row) => {
+        addRow += row[i];
+      });
+
+      verticalArray.push(addRow);
+      addRow = "";
+    }
+
+    let verticalReversedArray = [];
+
+    verticalArray.forEach((row) => {
+      const reversedRow = [...row].reverse().join("");
+      verticalReversedArray.push(reversedRow);
+    });
+
+    let startX;
+    let endX;
+    let startY;
+    let endY;
+    console.log(verticalReversedArray);
+
+    verticalReversedArray.forEach((row, index) => {
+      if (row.search(word) > -1) {
+        console.log(word);
+        startX = index + 1;
+        endX = index + 1;
+        startY = row.length - row.search(word);
+        endY = startY - word.length + 1;
+      }
+    });
+
+    if (startX > 0) {
+      return { start: [startY, startX], end: [endY, endX] };
+    } else return;
+  }
+
   find(words) {
     let resultsObject = {};
 
@@ -95,6 +138,7 @@ class WordSearch {
       const matchingWord = this.searcherHorizontal(word);
       const matchingWordReversed = this.searcherHorizontalReversed(word);
       const matchingWordVertical = this.searcherTopBottom(word);
+      const matchingWordVerticalReversed = this.searcherBottomTop(word);
 
       if (matchingWord !== undefined) {
         resultsObject[word] = matchingWord;
@@ -102,6 +146,8 @@ class WordSearch {
         resultsObject[word] = matchingWordReversed;
       } else if (matchingWordVertical !== undefined) {
         resultsObject[word] = matchingWordVertical;
+      } else if (matchingWordVerticalReversed !== undefined) {
+        resultsObject[word] = matchingWordVerticalReversed;
       } else resultsObject[word] = undefined;
     });
     console.log(resultsObject);
@@ -110,16 +156,10 @@ class WordSearch {
 }
 
 const wordSearch = new WordSearch([
-  "abcdefghij", // j
-  "saggsasaga", // a
-  "qwopjrpqiv", // v
-  "mfaslkfmsa", // a
-  "msflksmafl",
-  "oqjrwqiwra",
-  "ijqwrjifmm",
-  "mafslkfasm",
-  "koqwopwqrk",
-  "kvuvyfbjch",
+  'd___',
+  'r___',
+  'o___',
+  'w___',
 ]);
 
 wordSearch.find(["pyton", "java", "word"]);
